896681fcd290cb9fa8acdfe6903a5e2ab66fa223
Hiren Bhuriya <hirenbhuriya@gmail.com>
2016-08-28 17:46:02 +0530

    form

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-
 var express = require ('express');
 var app = express();
 
 app.get('/',function (req,res)
  {
-
        console.log(req.query.name);
-
        res.send(`
-
-
+         <html>
+         <body>
          <form action = '/home'  method = get />
          enter name : <input type = "text" name = "name">
-
          <input type = "submit" value = "submit" >
-
-       
-
+         </body>
+         </html>
          `)
 
 app.get('/home',function (req,res)
  {
-
-        res.send("Hello world"  + req.query.name );
-
+        res.send("hello world" + req.query.name );
 })
-
-
 });
-
 app.listen(3000,function ()
  {
            console.log("hello world");
-
 })
