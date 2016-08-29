456674a1c2082878093683bd411d83c0f9a38c44
Hiren Bhuriya <hirenbhuriya@gmail.com>
2016-08-30 01:28:25 +0530

    name and age

--- app.js
+++ app.js
@@ -3,23 +3,65 @@ var app = express();
 
 app.get('/',function (req,res)
  {
-       console.log(req.query.name);
-       res.send(`
-         <html>
-         <body>
-         <form action = '/home'  method = get />
-         enter name : <input type = "text" name = "name">
-         <input type = "submit" value = "submit" >
-         </body>
-         </html>
-         `)
 
-app.get('/home',function (req,res)
- {
-        res.send("hello world" + req.query.name );
-})
-});
+       console.log(req.query);
+
+        res.send(`
+          <html>
+          <body>
+          <form action = '/welcome'  method = get />
+           Enter Name : <input type = "text" name = "name"><br/>
+           Enter year  : <input type = "number" name = "age">
+          <input type = "submit" value = "submit" >
+          </body>
+          </html>
+          `)
+
+ app.get('/welcome',function (req,res)
+  {
+     console.log(req.query);
+     var a = 2016;
+     var age = a - req.query.age
+    //res.send(`Hello world ${req.query.name} - Age ${req.query.name}`)
+ res.send("Hello World"+ req.query.name + ". " +"You are" + " " + age + " " +"Yers old.");
+
+ })
+ });
+
+
+
+
+
+
+//
+// app.get('/date',function(req,res){
+//
+// var year = 2016;
+// //var age = year
+//
+//   res.send(`
+//          <html>
+//          <body>
+//          <form action = '/date'  method = get />
+//          Enter Name : <input type = "text" name = "name"><br/>
+//          Enter birthdate : <input type = "text" name = "year">
+//          <input type = "submit" value = "submit" >
+//         </body>
+//         </html>
+//
+//
+//           `)
+// });
+
+
+
+
+
+
+
+
+
 app.listen(3000,function ()
  {
            console.log("hello world");
-})
+});
